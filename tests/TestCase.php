7920015d5a512d67fabe33f9c5d51e02bd837e7b
Vladimir Ilin <mark09072010@gmail.com>
2018-10-29 11:27:29 +0300

    add script for linter and fix linter errors

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,4 +1,5 @@
 <?php
+namespace App\Tests;
 
 abstract class TestCase extends Laravel\Lumen\Testing\TestCase
 {
@@ -9,6 +10,6 @@ abstract class TestCase extends Laravel\Lumen\Testing\TestCase
      */
     public function createApplication()
     {
-        return require __DIR__.'/../bootstrap/app.php';
+        return require __DIR__ . '/../bootstrap/app.php';
     }
 }
