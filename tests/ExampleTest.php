c1de1498452fd45749c91e887af6350b47e636fa
Vladimir Ilin <mark09072010@gmail.com>
2018-10-29 12:44:48 +0300

    fix travis

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace Tests\Example;
+namespace Tests;
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
