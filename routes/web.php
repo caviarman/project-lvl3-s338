a9a2e6669a46510674c54ad0acff9b4da1345d64
Vladimir Ilin <mark09072010@gmail.com>
2018-10-31 00:39:59 +0300

    add view domain

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,22 @@ $router->get('/', function () {
 });
 
 $router->post('/domains', function (Request $request) {
+    $id = ((int)(get_object_vars(DB::select('select id from domains order by id desc limit 1')[0]))['id']) + 1;
     $name = $request->input('address');
     $this->validate($request, ['address' => 'active_url']);
     $time = Carbon::now()->toRfc2822String();
-    DB::insert('insert into domains (name, created_at, updated_at) values (?,?,?)', [$name, $time, $time]);
-    return $name;
+    DB::insert('insert into domains (id, name, created_at, updated_at) values (?,?,?,?)', [$id, $name, $time, $time]);
+    //$id = DB::table('domains')->insertGetId(
+    //    [
+    //        'name' => $name,
+    //        'created_at' => $time,
+    //        'updated_at' => $time
+    //    ]
+    //);
+    redirect()->route('domains.show', ['id' => $id]);
 });
+
+$router->get('domains/{id}', ['as' => 'domains.show', function ($id) {
+    //$address = get_object_vars(DB::select('select * from domains where id = ?', [$id]));
+    return 'sucsess';
+}]);
