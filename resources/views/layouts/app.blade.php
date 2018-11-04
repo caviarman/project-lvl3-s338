fdfa99f2f9c33f9884709627814a8a97e4209a1a
Vladimir Ilin <mark09072010@gmail.com>
2018-11-04 15:20:32 +0300

    fix mobile view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,24 +16,15 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Menu</a>
-    <button 
-      class="navbar-toggler" 
-      type="button" 
-      data-toggle="collapse" 
-      data-target="#navbarNav" 
-      aria-controls="navbarNav" 
-      aria-expanded="false" 
-      aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <a class="navbar-brand" href="{{ route('domains.main') }}">Home</a>
+    <a class="navbar-brand" href="{{ route('domains.all') }}">Domains</a>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="{{ route('domains.main') }}">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ route('domains.all') }}">Domains</a>
+          <a class="nav-link" href="#"></a>
         </li>
       </ul>
     </div>
